364562525139cc68891fac599acb9e679192c6ff
JohanyF <johanyf627@gmail.com>
2024-09-04 01:10:00 -0500

    Formatted code

--- script.js
+++ script.js
@@ -2,123 +2,118 @@ const READ_COLOR = "#a8ff69";
 const NOT_READ_COLOR = "#ff6969";
 
 class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 class Library {
-    constructor() {
-        this.myLibrary = []
-    }
+  constructor() {
+    this.myLibrary = [];
+  }
 
-    get getlibraryArr() {
-        return this.myLibrary;
-    }
+  get getlibraryArr() {
+    return this.myLibrary;
+  }
 
-    addBookToLibrary(title, author, pages, read) {
-        let myBook = new Book(title, author, pages, read);
-        this.myLibrary.push(myBook);
-        createCard(this.myLibrary[this.myLibrary.length-1]);
-    }
-    
-    // Create a function that displays all of the books that are already preloaded in the array
-    addPreLoadedBooks() {
-        this.myLibrary.forEach((book) => {
-            createCard(book);
-        })
-    }
-    
+  addBookToLibrary(title, author, pages, read) {
+    let myBook = new Book(title, author, pages, read);
+    this.myLibrary.push(myBook);
+    createCard(this.myLibrary[this.myLibrary.length - 1]);
+  }
+
+  // Create a function that displays all of the books that are already preloaded in the array
+  addPreLoadedBooks() {
+    this.myLibrary.forEach((book) => {
+      createCard(book);
+    });
+  }
 }
 
 function createCard(obj) {
-    const cardContainer = document.querySelector(".cards-container");
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.setAttribute('id', library.getlibraryArr.indexOf(obj));
+  const cardContainer = document.querySelector(".cards-container");
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.setAttribute("id", library.getlibraryArr.indexOf(obj));
 
-    cardContainer.appendChild(card);
+  cardContainer.appendChild(card);
 
-    const bookTitle = document.createElement("h2");
-    bookTitle.textContent = obj.title;
-    card.appendChild(bookTitle);
+  const bookTitle = document.createElement("h2");
+  bookTitle.textContent = obj.title;
+  card.appendChild(bookTitle);
 
-    const authorName = document.createElement("i");
-    authorName.textContent = `by ${obj.author}`;
-    card.appendChild(authorName);
+  const authorName = document.createElement("i");
+  authorName.textContent = `by ${obj.author}`;
+  card.appendChild(authorName);
 
-    const totalPages = document.createElement("p");
-    totalPages.classList.add("flex-one");
-    totalPages.textContent = `${obj.pages} pages`;
-    card.appendChild(totalPages);
+  const totalPages = document.createElement("p");
+  totalPages.classList.add("flex-one");
+  totalPages.textContent = `${obj.pages} pages`;
+  card.appendChild(totalPages);
 
-    const cardBtns = document.createElement("div");
-    cardBtns.classList.add("card-buttons")
-    card.appendChild(cardBtns);
+  const cardBtns = document.createElement("div");
+  cardBtns.classList.add("card-buttons");
+  card.appendChild(cardBtns);
 
-    const readBtn = document.createElement("button");
-    readBtn.setAttribute("type", "button");
-    readBtn.classList.add("card-btn");
-    // console.log(obj);
-    if(obj.read === true) {
-        readBtn.textContent = "Read";
-        readBtn.style.backgroundColor = READ_COLOR;
+  const readBtn = document.createElement("button");
+  readBtn.setAttribute("type", "button");
+  readBtn.classList.add("card-btn");
+  // console.log(obj);
+  if (obj.read === true) {
+    readBtn.textContent = "Read";
+    readBtn.style.backgroundColor = READ_COLOR;
+  } else {
+    readBtn.textContent = "Not Read";
+    readBtn.style.backgroundColor = NOT_READ_COLOR;
+  }
+  cardBtns.appendChild(readBtn);
 
+  // Toggles between Read (true) and Not Read (false)
+  readBtn.addEventListener("click", () => {
+    obj.read = !obj.read;
+    if (obj.read === true) {
+      readBtn.textContent = "Read";
+      readBtn.style.backgroundColor = READ_COLOR;
     } else {
-        readBtn.textContent = "Not Read";
-        readBtn.style.backgroundColor = NOT_READ_COLOR;
-
+      readBtn.textContent = "Not Read";
+      readBtn.style.backgroundColor = NOT_READ_COLOR;
     }
-    cardBtns.appendChild(readBtn);
+  });
 
-    // Toggles between Read (true) and Not Read (false)
-    readBtn.addEventListener("click", () => {
-        obj.read = !obj.read;
-        if(obj.read === true) {
-            readBtn.textContent = "Read";
-            readBtn.style.backgroundColor = READ_COLOR;
-    
-        } else {
-            readBtn.textContent = "Not Read";
-            readBtn.style.backgroundColor = NOT_READ_COLOR;
-    
-        }
-    })
+  const deleteBtn = document.createElement("button");
+  deleteBtn.setAttribute("type", "button");
+  deleteBtn.setAttribute("id", "removeBtn");
+  deleteBtn.classList.add("card-btn");
+  deleteBtn.textContent = "Remove";
+  cardBtns.appendChild(deleteBtn);
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("type", "button");
-    deleteBtn.setAttribute("id", "removeBtn");
-    deleteBtn.classList.add("card-btn");
-    deleteBtn.textContent = "Remove";
-    cardBtns.appendChild(deleteBtn);
-
-
-    // Delete book 
-    deleteBtn.addEventListener("click", () => {
-        const removeCard = document.getElementById(`${library.getlibraryArr.indexOf(obj)}`);
-        removeCard.remove();
-        library.getlibraryArr.splice(library.getlibraryArr.indexOf(obj), 1);
-        updateBookIDs();
-    })
-
+  // Delete book
+  deleteBtn.addEventListener("click", () => {
+    const removeCard = document.getElementById(
+      `${library.getlibraryArr.indexOf(obj)}`
+    );
+    removeCard.remove();
+    library.getlibraryArr.splice(library.getlibraryArr.indexOf(obj), 1);
+    updateBookIDs();
+  });
 }
 
 // Function updates each book id after one is removed
 function updateBookIDs() {
-    const bookCard = document.querySelectorAll(".card");
-    bookCard.forEach((card, index=0) => {
-        card.setAttribute('id', `${index}`);
-        index++;
-    })  
+  const bookCard = document.querySelectorAll(".card");
+  bookCard.forEach((card, index = 0) => {
+    card.setAttribute("id", `${index}`);
+    index++;
+  });
 }
 
 const modal = document.querySelector("#modal");
 const addBtn = document.querySelector(".add-btn");
-const submitBtn = document.querySelector(".submit-btn")
-const cancelBtn = document.querySelector(".cancel-btn")
+const submitBtn = document.querySelector(".submit-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
 
 const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
@@ -126,26 +121,34 @@ const pagesInput = document.querySelector("#pages");
 const readInput = document.querySelector("#read");
 
 addBtn.addEventListener("click", () => {
-    modal.showModal();
+  modal.showModal();
 });
 
-cancelBtn.addEventListener("click" , (event) => {
-    event.preventDefault();
-    modal.close();
-    document.bookForm.reset();
-})
+cancelBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.close();
+  document.bookForm.reset();
+});
 
 submitBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    library.addBookToLibrary(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
-    modal.close();
-    document.bookForm.reset();
-})
-
-
+  event.preventDefault();
+  library.addBookToLibrary(
+    titleInput.value,
+    authorInput.value,
+    pagesInput.value,
+    readInput.checked
+  );
+  modal.close();
+  document.bookForm.reset();
+});
 
 const library = new Library();
 
 library.addBookToLibrary("1984", "George Orwell", 328, true);
-library.addBookToLibrary("The Strange Case of Origami Yoda", "Tom Angleberger", 141, true);
+library.addBookToLibrary(
+  "The Strange Case of Origami Yoda",
+  "Tom Angleberger",
+  141,
+  true
+);
 library.addBookToLibrary("Diary of a Wimpy Kid", "Jeff Kinney", 221, false);
